6822c6198ad632dd7d68a2905c3e404498aa43ca
Paul-Louis Pröve <mail@plpp.de>
2025-05-23 11:16:58 +0200

    removed old ie stuff, increased performance for font loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,27 +31,6 @@
 			}, 100);
 		});
 
-	// Fix: Flexbox min-height bug on IE.
-		if (browser.name == 'ie') {
-
-			var flexboxFixTimeoutId;
-
-			$window.on('resize.flexbox-fix', function() {
-
-				clearTimeout(flexboxFixTimeoutId);
-
-				flexboxFixTimeoutId = setTimeout(function() {
-
-					if ($wrapper.prop('scrollHeight') > $window.height())
-						$wrapper.css('height', 'auto');
-					else
-						$wrapper.css('height', '100vh');
-
-				}, 250);
-
-			}).triggerHandler('resize.flexbox-fix');
-
-		}
 
 	// Nav.
 		var $nav = $header.children('nav'),
